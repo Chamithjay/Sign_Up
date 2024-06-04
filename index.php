41f90e3c0937bea288b2cd9c30269b0864c6e068
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-06-04 22:11:13 +0530

    Sign In

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div id=form>
+        <form action="database.php" method="GET">
+            <div id="FirstName">
+                <label>First Name</label>
+                <input type="text" name="FirstName" id='FirstName' placeholder='First Name'>
+            </div>
+            <div id="LastName">
+                <label>Last Name</label>
+                <input type="text" name="LastName" id='LastName' placeholder='Last Name'>
+            </div>
+            <div id="FirstName">
+                <label>First Name</label>
+                <input type="text" name="FirstName" id='FirstName' placeholder='First Name'>
+            </div>
+            <div id="FirstName">
+                <label>First Name</label>
+                <input type="text" name="FirstName" id='FirstName' placeholder='First Name'>
+            </div>
+            
+
+    
+</body>
+</html>
