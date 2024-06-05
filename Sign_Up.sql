e193ea1b2b2a02d42cadad70adae2c099b874549
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-06-05 14:17:07 +0530

    day2

---
+++ Sign_Up.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Sign_Up;
+
+use sign_up;
+CREATE TABLE sign_up(
+	firstname VARCHAR(10),
+    lastname VARCHAR(10),
+    pwd VARCHAR(100),
+    date_of_birth DATE,
+    gender ENUM('Male','Female','Other')
+);
